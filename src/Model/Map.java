685bf5ff76f6bb78812cc448ca962115c8ae8c7f
mhmazidi4 <mhmazidi4@gmail.com>
2018-11-27 18:07:26 +0330

    Yet at the beginning

--- src/Model/Map.java
+++ src/Model/Map.java
@@ -1,14 +1,39 @@
 package Model;
 
-public class MapVeiw implements Printable {
+import java.util.ArrayList;
 
-    String name;
+public class Map implements Printable{
 
-    public void view(){
+
+
+
+    Grass grass;
+
+    private static Map ourInstance =new Map();
+    private static final int Num_Of_CELLS_IN_ROW=5;
+    private static final int Num_Of_CELLS_IN_COLOUM=5;
+    Cell[][] cells =new Cell[Num_Of_CELLS_IN_COLOUM][Num_Of_CELLS_IN_ROW];
+
+    private Map() {
+        this.cells = cells;
+    }
+    public static public static Map getInstance() {
+        return ourInstance;
+    }
+
+    public ArrayList<Cell> getAjacentCells(Cell cell){
 
     }
 
 
+    public boolean cage(Position position){
+        return true;
+    }
 
+
+    public void turn(){
+
+
+    }
 
 }
