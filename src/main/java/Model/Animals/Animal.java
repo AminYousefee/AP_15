c6d1a0178ea26eeaa5b075b64e260159304b2872
Amin <yousefizadeamin@gmail.com>
2018-12-14 22:19:13 +0330

    all move methods created

--- src/main/java/Model/Animals/Animal.java
+++ src/main/java/Model/Animals/Animal.java
@@ -1,12 +1,16 @@
 package Model.Animals;
 
+import Model.Cell;
 import Model.Item;
+import Model.Positions.Position;
 import Model.Upgradable;
+import java.util.Random;
 
 public abstract class Animal extends Item implements Upgradable {
 
     public static final AnimalInfo Cat_Info;
     public static final AnimalInfo Dog_Info;
+    private Cell cell;
 
     static {
         Cat_Info = new AnimalInfo();
@@ -14,8 +18,8 @@ public abstract class Animal extends Item implements Upgradable {
         Dog_Info = new AnimalInfo();
     }
 
-    int fullness;
-    int level;
+    private int fullness;
+    private int level;
 
     public static AnimalInfo findAnimalType(String name) {
         if (name.equalsIgnoreCase("cat")) {
@@ -28,8 +32,78 @@ public abstract class Animal extends Item implements Upgradable {
 
 
     }
+    private void moveVertical(boolean up) {
+        int y = this.cell.getPosition().getY();
+        int x = this.cell.getPosition().getX();
+        if (up) {
+            this.cell.setPosition(new Position(x, y + 1));
+        } else {
+            this.cell.setPosition(new Position(x, y - 1));
+        }
+    }
+    private void moveHorizontal(boolean right){
+        int y = this.cell.getPosition().getY();
+        int x = this.cell.getPosition().getX();
+        if (right) {
+            this.cell.setPosition(new Position(x + 1, y));
+        } else {
+            this.cell.setPosition(new Position(x - 1, y));
+        }
+    }
 
-    public abstract boolean move();
+    private void goTo(int i, int j) {
+        int horizontal = j - this.cell.getPosition().getX();
+        int vertical = i - this.cell.getPosition().getY();
+        boolean up, right;
+        if (horizontal > 0) {
+            right = true;
+        }else {
+            right = false;
+        }
+        for (int h = 0; h < Math.abs(horizontal); h++) {
+            moveHorizontal(right);
+        }
+        if (vertical > 0) {
+            up = true;
+        }else{
+            up = false;
+        }
+        for (int k = 0; k < Math.abs(vertical); k++) {
+            moveVertical(up);
+        }
+    }
+
+    private int[] nearestGrass() {
+        int myX = this.cell.getPosition().getX();
+        int myY = this.cell.getPosition().getY();
+
+    }
+
+    public void move(){
+        if (this.fullness < 30) {
+            int[] target = this.nearestGrass();
+            int xTarget = target[0];
+            int yTarget = target[1];
+            this.goTo(xTarget,yTarget);
+        } else {
+            Random random = new Random();
+            int direct = random.nextInt(5);
+            switch (direct) {
+                case 1:
+                    this.moveVertical(true);
+                    break;
+                case 2:
+                    this.moveVertical(false);
+                    break;
+                case 3:
+                    this.moveHorizontal(true);
+                    break;
+                case 4:
+                    this.moveHorizontal(false);
+            }
+        }
+    }
+
 
 
     private boolean getSpeed() {
@@ -37,9 +111,32 @@ public abstract class Animal extends Item implements Upgradable {
 
     }
 
+    public Cell getCell() {
+        return cell;
+    }
+
+    public void setCell(Cell cell) {
+        this.cell = cell;
+    }
 
     private boolean eat() {
         return false;
+    }
+
+    public int getFullness() {
+        return fullness;
+    }
+
+    public void setFullness(int fullness) {
+        this.fullness = fullness;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
     }
 
     public static class AnimalInfo {
