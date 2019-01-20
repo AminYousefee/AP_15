a2638357e6d628e50e1838744998aa51d05fb68e
mhmazidi4 <mhmazidi4@gmail.com>
2019-01-20 12:30:04 +0330

    to check a feature

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -16,12 +16,20 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main extends Application {
     public static GridPane gridPane;
+    public static HashMap<String, Image> loadedImages;
+
+    static {
+        File currentDir = new File("./static");
+        generateImages(currentDir);
+    }
 
     public static void main(String[] args) {
         launch(args);
@@ -46,6 +54,110 @@ public class Main extends Application {
         }
 
     }
+
+    static Scene getMainMenuScene(int width, int height) {
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        pane.setHgap(10);
+        pane.setVgap(10);
+        pane.setPadding(new Insets(20, 20, 20, 20));
+
+        Button btn1 = new Button("Single Game");
+        btn1.setOnAction(actionEvent -> {
+            //todo
+        });
+        pane.add(btn1, 0, 1);
+
+        Button btn2 = new Button("Multiplayer");
+        btn2.setOnAction(actionEvent -> {
+            //todo
+        });
+        pane.add(btn2, 1, 1);
+
+        Button btn3 = new Button("Exit");
+        btn3.setOnAction(actionEvent -> {
+            //todo
+        });
+        pane.add(btn3, 2, 1);
+
+        String backpath = new String("./static/back.png")
+        FileInputStream input = new FileInputStream(backpath);
+        Image image = new Image(input);
+        BackgroundImage backgroundImage = new BackgroundImage(image,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+        Background background = new Background(backgroundImage);
+
+        pane.setBackground(background);
+
+        Scene scene = new Scene(pane, width, height);
+        return scene;
+    }
+
+
+/*
+    public static void controller.Main(String[] args) {
+        ArrayList<Mission.Goal.EE> ees = new ArrayList<>(0);
+        ees.add(new Mission.Goal.EE(Item.getInstance("Egg").getItemInfo(), 5));
+        ees.add(new Mission.Goal.EE(Item.getInstance("Horn").getItemInfo(), 10));
+        Mission mission1 = new Mission(new Mission.Goal(ees));
+        Farm farm1 = new Farm();
+        Game game = new Game("Empty", mission1, farm1);
+        InputProcessor.game =game;
+
+
+
+        //test 1
+        */
+/*InputProcessor.game.getFarm().getMap().getCellByPosition(new MapPosition(2,2)).addItem(WildAnimal.getInstance("Lion"));
+        InputProcessor.game.getFarm().getMap().getCellByPosition(new MapPosition(2,2)).addItem(WildAnimal.getInstance("bear"));
+        InputProcessor.game.getFarm().getMap().getCellByPosition(new MapPosition(2,2)).addItem(ProductiveAnimal.getInstance("Turkey"));
+        //game.getFarm().
+        game.getFarm().getMap().getCellByPosition(new MapPosition(2,2)).turn();
+
+*//*
+
+
+
+
+
+
+
+
+    }
+*/
+
+    private static void generateImages(File dir) {
+
+        File[] files = dir.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                generateImages(file);
+            } else {
+
+            }
+        }
+    }
+
+
+    /*static Scene singleGameScene(int width, int height) {
+        GridPane pane = new GridPane();
+        String backpath = new String("./static/back.png")
+        FileInputStream input = new FileInputStream(backpath);
+        Image image = new Image(input);
+        BackgroundImage backgroundImage = new BackgroundImage(image,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+        Background background = new Background(backgroundImage);
+
+
+
+
+    }*/
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -83,7 +195,6 @@ public class Main extends Application {
         stage.show();
     }
 
-
     private void gameStarts(Stage mainStage) {
         GridPane gridPane = new GridPane();
         GridPane pane = new GridPane();
@@ -100,38 +211,6 @@ public class Main extends Application {
 
     }
 
-
-/*
-    public static void controller.Main(String[] args) {
-        ArrayList<Mission.Goal.EE> ees = new ArrayList<>(0);
-        ees.add(new Mission.Goal.EE(Item.getInstance("Egg").getItemInfo(), 5));
-        ees.add(new Mission.Goal.EE(Item.getInstance("Horn").getItemInfo(), 10));
-        Mission mission1 = new Mission(new Mission.Goal(ees));
-        Farm farm1 = new Farm();
-        Game game = new Game("Empty", mission1, farm1);
-        InputProcessor.game =game;
-
-
-
-        //test 1
-        */
-/*InputProcessor.game.getFarm().getMap().getCellByPosition(new MapPosition(2,2)).addItem(WildAnimal.getInstance("Lion"));
-        InputProcessor.game.getFarm().getMap().getCellByPosition(new MapPosition(2,2)).addItem(WildAnimal.getInstance("bear"));
-        InputProcessor.game.getFarm().getMap().getCellByPosition(new MapPosition(2,2)).addItem(ProductiveAnimal.getInstance("Turkey"));
-        //game.getFarm().
-        game.getFarm().getMap().getCellByPosition(new MapPosition(2,2)).turn();
-
-*//*
-
-
-
-
-
-
-
-
-    }
-*/
-
-
 }
+
+
